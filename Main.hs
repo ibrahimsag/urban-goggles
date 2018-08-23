cf310d57d4915842426a666295f2ff9176965b68
Ibrahim Sagiroglu <sag.ibrahim@gmail.com>
2018-08-23 12:21:18 +0300

    parser

--- Main.hs
+++ Main.hs
@@ -1,9 +1,13 @@
 module Main where
 
+import Data.Functor.Identity(Identity)
+
+import Text.Parsec
 import Text.Parsec.String (Parser)
 import Text.Parsec.Language (emptyDef)
 
 import qualified Text.Parsec.Token as Tok
+import qualified Text.Parsec.Expr as Ex
 
 -- # Lexer
 
@@ -52,6 +56,95 @@ data Expr
   | Extern Name [Name]
   | BinaryOp Name Expr Expr
   deriving (Eq, Ord, Show)
+
+-- # Parser
+
+binary
+  :: String
+  -> Ex.Assoc
+  -> Ex.Operator String () Identity Expr
+binary s assoc = Ex.Infix (reservedOp s >> return (BinaryOp s)) assoc
+
+table :: [[Ex.Operator String () Identity Expr]]
+table = [ [ binary "*" Ex.AssocLeft
+          , binary "/" Ex.AssocLeft
+          ]
+        , [ binary "+" Ex.AssocLeft
+          , binary "-" Ex.AssocLeft
+          ]
+        ]
+
+int :: Parser Expr
+int = do
+  n <- integer
+  return (Float (fromInteger n))
+
+floating :: Parser Expr
+floating = do
+  n <- float
+  return (Float n)
+
+expr :: Parser Expr
+expr = Ex.buildExpressionParser table factor
+
+variable :: Parser Expr
+variable = do
+  var <- identifier
+  return (Var var)
+
+function :: Parser Expr
+function = do
+  reserved "def"
+  name <- identifier
+  args <- parens (many identifier)
+  body <- expr
+  return (Function name args body)
+
+extern :: Parser Expr
+extern = do
+  reserved "extern"
+  name <- identifier
+  args <- parens (many identifier)
+  return (Extern name args)
+
+call :: Parser Expr
+call = do
+  name <- identifier
+  args <- parens $ commaSep expr
+  return (Call name args)
+
+factor :: Parser Expr
+factor = try floating
+      <|> try int
+      <|> try extern
+      <|> try function
+      <|> try call
+      <|> variable
+      <|> parens expr
+
+defn :: Parser Expr
+defn = try extern
+   <|> try function
+   <|> expr
+
+contents :: Parser a -> Parser a
+contents p = do
+  Tok.whiteSpace lexer
+  r <- p
+  eof
+  return r
+
+toplevel :: Parser [Expr]
+toplevel = many $ do
+  def <- defn
+  reservedOp ";"
+  return def
+
+parseExpr :: String -> Either ParseError Expr
+parseExpr s = parse (contents expr) "<stdin>" s
+
+parseToplevel :: String -> Either ParseError [Expr]
+parseToplevel s = parse (contents toplevel) "<stdin>" s
 
 
 main :: IO ()
