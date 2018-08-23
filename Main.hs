ca21123ecd9921e8b89fcdb5f5a3a7808de1100a
Ibrahim Sagiroglu <sag.ibrahim@gmail.com>
2018-08-23 12:05:28 +0300

    lexer

--- Main.hs
+++ Main.hs
@@ -1,4 +1,42 @@
 module Main where
 
+import Text.Parsec.String (Parser)
+import Text.Parsec.Language (emptyDef)
+
+import qualified Text.Parsec.Token as Tok
+
+lexer :: Tok.TokenParser ()
+lexer = Tok.makeTokenParser style
+  where
+    style = emptyDef {
+        Tok.commentLine = "#"
+      , Tok.reservedOpNames = ["+", "*", "-", "/", ";", ",", "<"]
+      , Tok.reservedNames = ["def", "extern"]
+      }
+
+integer :: Parser Integer
+integer = Tok.integer lexer
+
+float :: Parser Double
+float = Tok.float lexer
+
+parens :: Parser a -> Parser a
+parens = Tok.parens lexer
+
+commaSep :: Parser a -> Parser [a]
+commaSep = Tok.commaSep lexer
+
+semiSep :: Parser a -> Parser [a]
+semiSep = Tok.semiSep lexer
+
+identifier :: Parser String
+identifier = Tok.identifier lexer
+
+reserved :: String -> Parser ()
+reserved = Tok.reserved lexer
+
+reservedOp :: String -> Parser ()
+reservedOp = Tok.reservedOp lexer
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
