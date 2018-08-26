837619c6cc0248675117dc397640bfad947e0f56
Ibrahim Sagiroglu <sag.ibrahim@gmail.com>
2018-08-26 17:40:18 +0300

    generate IR

--- Main.hs
+++ Main.hs
@@ -1,10 +1,20 @@
 module Main where
 
 import System.Console.Haskeline
+import System.Environment
+
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Short as BSS
+
+import Data.List (foldl')
+import Data.Map (Map)
+import qualified Data.Map as Map
 
 import Data.Functor.Identity(Identity)
+import Data.Maybe (maybe)
 
 import Control.Monad.Trans
+import Control.Monad.State.Strict
 
 import Text.Parsec
 import Text.Parsec.String (Parser)
@@ -12,6 +22,12 @@ import Text.Parsec.Language (emptyDef)
 
 import qualified Text.Parsec.Token as Tok
 import qualified Text.Parsec.Expr as Ex
+
+import qualified LLVM.Context as LLContext
+import qualified LLVM.Module as LLModule
+import qualified LLVM.AST as LLAST
+import qualified LLVM.AST.FloatingPointPredicate as LLAST
+import qualified LLVM.IRBuilder as IRB
 
 -- # Lexer
 
@@ -158,15 +174,99 @@ parseExpr s = parse (contents expr) "<stdin>" s
 parseToplevel :: String -> Either ParseError [Phrase]
 parseToplevel s = parse (contents toplevel) "<stdin>" s
 
--- # RPPL
+-- # Codegen
 
-process :: [Phrase] -> IO ()
-process expr = mapM_ print expr
+doubleTy :: LLAST.Type
+doubleTy = LLAST.FloatingPointType LLAST.DoubleFP
 
-main :: IO ()
-main = runInputT defaultSettings loop
+data CodegenState = CodegenState {
+    symbolTable :: Map Name LLAST.Operand
+  , functionTable :: Map Name LLAST.Operand
+  , modDefinitions :: [LLAST.Definition]
+  }
+
+emptyCodegen :: CodegenState
+emptyCodegen = CodegenState { symbolTable = Map.empty, functionTable = Map.empty, modDefinitions = []}
+
+type Codegen =  (StateT CodegenState IO)
+
+codegenIR :: Expr -> IRB.IRBuilderT (IRB.ModuleBuilderT Codegen) LLAST.Operand
+codegenIR = \case
+  Float d         -> IRB.double d
+  Var name        -> maybe (error ("unknown variable: " ++ name)) id . Map.lookup name <$> gets symbolTable
+  BinaryOp op l r -> do
+    lo <- codegenIR l
+    ro <- codegenIR r
+    case op of
+      "+" -> IRB.fadd lo ro
+      "-" -> IRB.fsub lo ro
+      "*" -> IRB.fmul lo ro
+      "<" -> IRB.fcmp LLAST.ULT lo ro >>= (\operand -> IRB.uitofp operand doubleTy)
+      _   -> error ("unknown operator: " ++ op)
+  Call name args  ->  do
+    calleeOperand <- (maybe (error ("unknown function: " ++ name)) id . Map.lookup name ) <$> gets functionTable
+    IRB.call calleeOperand =<< traverse (fmap (,[]) . codegenIR) args
+
+codegenDefn :: Defn -> (IRB.ModuleBuilderT Codegen) LLAST.Operand
+codegenDefn = \case
+  Function name args body -> do
+    funcOperand <- IRB.function
+          (LLAST.Name (packShort name))
+          (fmap ((doubleTy,) . IRB.ParameterName . packShort) args)
+          doubleTy
+          $ \argOs -> do
+      localSymTable <- gets symbolTable
+      let updateSymTableWithArgs symT = foldl'
+                    (\symTable (name, arg) -> Map.insert name arg symTable)
+                    symT
+                    (zip args argOs)
+      modify $ \s -> s { symbolTable = updateSymTableWithArgs (symbolTable s)}
+      codegenIR body >>= IRB.ret
+      modify $ \s -> s { symbolTable = localSymTable }
+    modify (\s -> s { functionTable = Map.insert name funcOperand (functionTable s) })
+    return funcOperand
+  Extern name args -> do
+    extOperand <- IRB.extern (LLAST.Name (packShort name)) (fmap (const doubleTy) args) doubleTy
+    modify (\s -> s { functionTable = Map.insert name extOperand (functionTable s) })
+    return extOperand
+
+packShort = BSS.toShort . BS.pack
+
+
+printModulIR :: LLAST.Module -> IO ()
+printModulIR modul = LLContext.withContext $ \ctx -> do
+    llir <- LLModule.withModuleFromAST ctx modul LLModule.moduleLLVMAssembly
+    BS.putStrLn llir
+
+buildLLModule :: [Phrase] -> Codegen LLAST.Module
+buildLLModule phrases = do
+    modDefs <- gets modDefinitions
+    defs <- IRB.execModuleBuilderT IRB.emptyModuleBuilder builder
+    let updatedDefs = modDefs ++ defs
+    modify (\s -> s { modDefinitions = updatedDefs } )
+    IRB.buildModuleT (packShort "<stdin>") (traverse IRB.emitDefn updatedDefs)
   where
-    loop :: InputT IO ()
+    builder = mapM_ (codegenDefn . toAnon) phrases
+    toAnon (DefnPhrase f) = f
+    toAnon (ExprPhrase e) = Function "anon" [] e
+
+-- # RCPL
+
+process :: String -> Codegen ()
+process = either (liftIO . print) (buildLLModule >=> liftIO . printModulIR) . parseToplevel
+
+interactive :: Codegen ()
+interactive = runInputT defaultSettings loop
+  where
+    loop :: InputT Codegen ()
     loop = getInputLine "ready> " >>= \case
       Nothing -> outputStrLn "Goodbye."
-      Just input -> (liftIO $ (either print process . parseToplevel) input) >> loop
+      Just input -> (lift $ process input) >> loop
+
+evalWithEmptyCodegen :: Codegen () -> IO ()
+evalWithEmptyCodegen = flip evalStateT emptyCodegen
+
+main :: IO ()
+main = getArgs >>= \case
+  [] -> evalWithEmptyCodegen $ interactive
+  filename :_ -> readFile filename >>= evalWithEmptyCodegen . process
